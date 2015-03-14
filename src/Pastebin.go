cd70ebaf657245c265937c20a453a2a24a607138
rbrick <rbrickproductions123@gmail.com>
2015-03-14 12:57:03 -0500

    Initial Commit

---
+++ src/Pastebin.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "net/url"
+    "strconv"
+    "net/http"
+    "io/ioutil"
+)
+
+// The parameters that will be written when making the request
+var api_params = make(map[string]string)
+
+// The view status of the paste
+const (
+    PUBLIC = iota
+    UNLISTED
+    PRIVATE
+)
+
+func addParam(k, v string) {
+    api_params[k] = v
+}
+
+// Sets the API key in the params
+func SetAPIKey(api_key string) {
+    addParam("api_dev_key", api_key)
+}
+
+func SetContent(content string) {
+    addParam("api_paste_code", content)
+}
+
+func SetName(name string) {
+    addParam("api_paste_name", name)
+}
+
+func SetViewStatus(status int) {
+    addParam("api_paste_private", strconv.Itoa(status))
+}
+
+
+func Execute() (string, error) {
+    data := url.Values{}
+    for k, v := range api_params {
+        data.Add(k, v)
+    }
+
+    resp, err := http.PostForm("http://pastebin.com/api/api_post.php", data)
+
+    if err != nil {
+        panic(err)
+        return nil, err
+    }
+
+    ra, err1 := ioutil.ReadAll(resp.Body)
+
+    if err1 != nil {
+        panic(err1)
+        return nil, err1
+    }
+
+    return ra, nil
+}
+
+
+
+
+
