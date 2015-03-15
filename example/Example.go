b4d533f9b438e281add5818fba5ef36fc99c31de
Ryan <rbrick@users.noreply.github.com>
2015-03-14 23:57:18 -0500

    Update Example.go

--- example/Example.go
+++ example/Example.go
@@ -7,11 +7,11 @@ import (
 
 
 func main() {
-    pb.SetAPIKey("25006b3890b4a16b40847018c2c74fd5")
+    pb.SetAPIKey("REDACTED")
     pb.SetContent("Hello World!")
     pb.SetName("Example")
     pb.SetViewStatus(pb.PUBLIC)
-    pb.SetUserKey("99324fa628c831fcbcf8e37b34876ac7")
+    pb.SetUserKey("REDACTED")
     pb.SetExpireDate(pb.NEVER)
 
     url, err := pb.Execute()
